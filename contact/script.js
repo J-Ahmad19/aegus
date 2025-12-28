ff9c1e82640d156ced2ec23ca01cc378e25aa40c
sanzo <sanzofr62@gmail.com>
2025-12-28 18:52:17 +0530

    changed mobile menu

--- contact/script.js
+++ contact/script.js
@@ -1,3 +1,4 @@
+
 const lenis = new Lenis();
 function raf(time) {
     lenis.raf(time);
@@ -6,49 +7,29 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 const menu = document.querySelector(".header .menu");
-let isOpen = false; // track state
+const overlay = document.querySelector(".menu-overlay");
+const lines = document.querySelectorAll(".header .menu hr");
+const closeBtn = document.querySelector(".menu-close");
+
+let isOpen = false;
+
+function openMenu() {
+    gsap.to(lines[0], { rotate: 45, y: 6, duration: 0.25 });
+    gsap.to(lines[1], { rotate: -45, y: -6, duration: 0.25 });
+    gsap.to(overlay, { left: 0, duration: 0.7, ease: "power3.inOut" });
+    isOpen = true;
+}
+
+function closeMenu() {
+    gsap.to(lines[0], { rotate: 0, y: 0, duration: 0.25 });
+    gsap.to(lines[1], { rotate: 0, y: 0, duration: 0.25 });
+    gsap.to(overlay, { left: "100%", duration: 0.7, ease: "power3.inOut" });
+    isOpen = false;
+}
 
 menu.addEventListener("click", () => {
-    if (!isOpen) {
-        // OPEN (turn into X)
-        gsap.to(".header .menu hr:nth-of-type(1)", {
-            rotate: 45,
-            y: 6,
-            duration: 0.3,
-            transformOrigin: "center"
-        });
-
-        gsap.to(".header .menu hr:nth-of-type(2)", {
-            rotate: -45,
-            y: -6,
-            duration: 0.3,
-            transformOrigin: "center"
-        });
-
-        gsap.to(".menu-overlay", {
-            left: "-30%",
-            ease: "power3.inOut",
-            duration: 1,
-        })
-    } else {
-        // CLOSE (reset to hamburger)
-        gsap.to(".header .menu hr:nth-of-type(1)", {
-            rotate: 0,
-            y: 0,
-            duration: 0.3
-        });
-
-        gsap.to(".header .menu hr:nth-of-type(2)", {
-            rotate: 0,
-            y: 0,
-            duration: 0.3
-        });
-        gsap.to(".menu-overlay", {
-            left: "-100%",
-            ease: "power3.inOut",
-            duration: 1,
-        })
-    }
-
-    isOpen = !isOpen;
+    isOpen ? closeMenu() : openMenu();
 });
+
+closeBtn.addEventListener("click", closeMenu);
+
